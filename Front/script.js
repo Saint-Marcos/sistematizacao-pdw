41670de87d952a99d1aded35e57f1ca3f48f8ff0
ESTUDAR <marcos.adriano@sempreceub.com>
2024-04-08 18:39:56 -0300

    adicionado flag de botão

--- Front/script.js
+++ Front/script.js
@@ -2,14 +2,19 @@ let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".navbar");
 let menuIcon = menu.querySelector("i");
 
+let isMenuClicked = false;
+
 menu.onclick = () => {
+  isMenuClicked = true;
   menuIcon.classList.toggle("fa-solid");
-  menuIcon.classList.toggle("fa-xmark");
+  menuIcon.classList.toggle("fa-x");
   navbar.classList.toggle("active");
 };
 
 window.onscroll = () => {
-  menuIcon.classList.remove("fa-solid");
-  menuIcon.classList.remove("fa-xmark");
-  navbar.classList.remove("active");
+  if (isMenuClicked) {
+    menuIcon.classList.remove("fa-solid");
+    menuIcon.classList.remove("fa-x");
+    navbar.classList.remove("active");
+  }
 };
